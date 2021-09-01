493ebc24b0b99842e933f7a328e6dd00ca7f8bc2
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-09-01 14:06:53 +0500

    third commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,18 +6,11 @@ export default class GameSavingLoader {
 
   static load() {
     read().then((response) => {
-      console.log(response);
-      console.log(json);
       json(response);
     }).then((result) => {
-      console.log(result);
-      //const gameSaving = new GameSaving(result);
-      return result;
+      return new GameSaving (result);
     }, (error) => {
       throw new Error(error.message);
     });
   }
 }
-
-const game = new GameSavingLoader;
-GameSavingLoader.load();
